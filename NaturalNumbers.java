75004e3fa83fc53f8c6eb0942cce4199841b37aa
obaturn <akintoyetoluwalase912@gmail.com>
2024-04-02 13:18:27 +0100

    added files

---
+++ NaturalNumbers.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class NaturalNumbers {
+
+public static void main(String [] args) {
+
+Scanner input = new Scanner(System.in);
+
+System.out.println("enter the first number : ");
+
+int number1 = input.nextInt();
+
+System.out.println("enter second number: ");
+
+int number2 = input.nextInt();
+
+System.out.println("enter third number : ");
+
+int number3 = input.nextInt();
+
+System.out.println("enter fouth number : ");
+
+int number4 = input.nextInt();
+
+System.out.println("enter fifth number : ");
+
+int number5 = input.nextInt();
+
+System.out.println("enter sixth number : ");
+
+int number6 = input.nextInt();
+
+System.out.println("enter seventh number : ");
+
+int number7 = input.nextInt();
+
+System.out.println("enter eight number : ");
+
+int number8 = input.nextInt();
+
+System.out.println("enter ninth number : ");
+
+int number9 = input.nextInt();
+
+System.out.println("enter tenth number : ");
+
+int number10 = input.nextInt();
+
+int sum = (number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10);
+
+System.out.println("the total sum is" + sum);
+
+}
+}
