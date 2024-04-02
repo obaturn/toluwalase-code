536fdee75fd1f4d77356acb3405851cc2d5b362e
obaturn <akintoyetoluwalase912@gmail.com>
2024-04-02 15:08:11 +0100

    added files

---
+++ LargestAndLowest2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class LargestAndLowest2 {
+
+public static void main(String [] args) {
+
+Scanner input = new Scanner(System.in);
+
+int smallest  = 0
+
+int largest  = 0
+
+int counter = 1
+
+System.out.println(enter a number)
+
+int num = input.nextInt();
+
+while (counter >= num) {
+
+if(largest number > num ) {
+
+largest = num
+}
+if(Smallest < num) {
+
+smallest = num
+}
+}
+System.out.println("the largest number is" + largest")
+
+System.out.println("the smallest number is" + smallest")
+}
+}
