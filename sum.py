8ec0b35da028098116174a96119286607654e395
obaturn <165762292+obaturn@users.noreply.github.com>
2024-04-06 19:29:33 +0100

    Add files via upload

--- sum.py
+++ sum.py
@@ -1,9 +1,12 @@
-number1 = int(input("enter first integer")),
-number2 = int(input("enter second integer")),
-print(' the addition of ', number1 , "and" , number2, 'is =' ,number1 + number2,
-       '/n the subtraction of ' , number1, " and" , number2, ' is = ' ,number1 - number2, 
-      '/n the divison of ' , number1, " and" , number2, 'is =' , number1 / number2,
-      '/n the multiplication of ', number1 ,"and" ,number2 , 'is = ' , number1 * number2,
-      '/n the whole number  divison of', number1, "and", number2 , 'is = ' ,number1//number2,
-      '/n the modulo number of', number1, "and" ,number2 ,'is =' , number1 % number2,)
-      
+num1 = int(input("enter first number:"))
+num2 = int(input("enter second number:"))
+num3 = int(input("enter third number:"))
+num4 = int(input("enter fourth number:"))
+num5 = int(input("enter fifth number:"))
+num6 = int(input(" enter sixth number:"))
+num7 = int(input(" enter seventh number:"))
+num8 = int(input("enter eigth number:"))
+num9 = int(input("enter ninth number:"))
+num10 = int(input("enter tenth number:"))
+sum = (num1 + num2 + num3 + num4 + num5 +  num6 + num7 + num8 + num9 + num10)
+print(sum)
